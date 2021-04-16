4a038d673c6d41de79ffe040f504926018842f1c
yannlo <73764168+yannlo@users.noreply.github.com>
2021-04-16 17:19:05 +0000

    init 2

---
+++ src/test/TestAffichage.java
@@ -0,0 +1,31 @@
+package test;
+
+
+
+import classList.* ;
+
+public class TestAffichage {
+
+
+	
+	public static void main(String[] args) {
+		
+		Classe c1 = new Classe("SIGL");
+		System.out.println(c1);
+		
+		Classe c2 = new Classe("TWIN");
+		System.out.println(c2);
+		
+		Classe c3 = new Classe("RTEL");
+		System.out.println(c3);
+		
+		Classe c4 = new Classe("SRIT");
+		System.out.println(c4);
+		
+		Auteur a2 = new Auteur("John Codeur");
+		System.out.println(a2);		
+
+
+	}
+
+}
